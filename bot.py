34bc9b4cb7a7f81bdd3f75d3c0de665d81afd284
vsiy <43692357+vsiy@users.noreply.github.com>
2022-08-19 19:06:42 +0000

    async ytdlp

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-import os, json
+import os, json, asyncio
 from urllib.parse import quote as urlencode
 
 import yt_dlp
@@ -20,6 +20,32 @@ async def on_ready():
 	for guild in client.guilds:
 		print(f'{guild.name} (id:{guild.id})')
 
+class Logger(object):
+	def __init__(self):
+		self.output_msgs = []
+
+	def push_to_output(self, msg):
+		ignored_msgs = [
+			": Downloading webpage",
+			": Downloading android player API JSON",
+			"/s ETA ",
+			"Deleting original file",
+			"(frag"
+		]
+		append = True
+		for line in ignored_msgs:
+			if line in msg:
+				append = False
+		if append:
+			self.output_msgs.append(msg)
+
+	def debug(self, msg):
+		self.push_to_output(msg)
+	def warning(self, msg):
+		self.push_to_output(msg)
+	def error(self, msg):
+		self.push_to_output(msg)
+
 @client.event
 async def on_message(message):
 	try:
@@ -32,45 +58,21 @@ async def on_message(message):
 			prog_msg_str = f'Attempting to DL: <{videourl}>'
 			prog_msg = await message.channel.send(prog_msg_str)
 
-			class Logger(object):
-				def __init__(self):
-					self.output_msgs = []
-
-				def push_to_output(self, msg):
-					ignored_msgs = [
-						": Downloading webpage",
-						": Downloading android player API JSON",
-						"/s ETA ",
-						"Deleting original file"
-					]
-					append = True
-					for line in ignored_msgs:
-						if line in msg:
-							append = False
-					if append:
-						self.output_msgs.append(msg)
-
-				def debug(self, msg):
-					self.push_to_output(msg)
-				def warning(self, msg):
-					self.push_to_output(msg)
-				def error(self, msg):
-					self.push_to_output(msg)
-
 			logger = Logger()
 
 			opts = ydl_opts.copy()
 			opts['logger'] = logger
 
-			with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-				info = ydl.extract_info(videourl, download=False)
+			loop = asyncio.get_running_loop()
+			with yt_dlp.YoutubeDL(opts) as ydl:
+				info = await loop.run_in_executor(None, lambda: ydl.extract_info(videourl, download=False))
 				title = info["title"]
 				msg_content = f"Attempting to DL: <{videourl}>\nTitle: {title}\nDownloading..."
 				await prog_msg.edit(content=msg_content)
 
-				info = ydl.extract_info(videourl, download=True)
+				info = await loop.run_in_executor(None, lambda: ydl.extract_info(videourl, download=True))
 				log = "\n".join(logger.output_msgs)
-				filename = ydl.prepare_filename(info)
+				filename = await loop.run_in_executor(None, lambda: ydl.prepare_filename(info))
 				if PUBLIC_ADDRESS and PUBLIC_ADDRESS != "YourPublicWebAddressHere":
 					filename = f"http://{PUBLIC_ADDRESS}/{urlencode(filename.replace('downloads/', ''))}"
 				msg_content += f"\n{log}\nOutput:\n{filename}"
