9f13998802ad39b0600c8b9284b20b935ec668c6
VS-W <30299529+VS-W@users.noreply.github.com>
2024-04-04 01:46:34 -0400

    cleanup output/error messages, edit message to show progress on downloads

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
-import os, json, asyncio
+import os, json, asyncio, time
 from base64 import urlsafe_b64encode
-from hashlib import sha256
+from hashlib import sha1
 from urllib.parse import quote as urlencode
 
 import yt_dlp
@@ -28,18 +28,25 @@ async def on_ready():
 		print(f'{guild.name} (id:{guild.id})', flush=True)
 
 class Logger(object):
-	def __init__(self):
+	def __init__(self, prog_msg, msg_content, async_loop):
 		self.output_msgs = []
+		self.valid_msgs = [msg_content]
+		self.prog_msg = prog_msg
+		self.async_loop = async_loop
+		self.time_since_last_edit = time.time()
 
 	def push_to_output(self, msg):
 		print(msg, flush=True)
+		self.output_msgs.append(msg)
+
 		ignored_msgs = [
 			": Downloading webpage",
 			": Downloading android player API JSON",
-			"/s ETA ",
 			"Deleting original file",
 			"(frag",
-			"[youtube]"
+			"[youtube]",
+			"[info] Downloading video thumbnail",
+			"[info] Video Thumbnail"
 		]
 		append = True
 		for line in ignored_msgs:
@@ -47,7 +54,23 @@ class Logger(object):
 				append = False
 		if append:
 			msg = msg.replace('_', '\_')
-			self.output_msgs.append(msg)
+
+			if "/s ETA " in msg:
+				if "/s ETA " in self.valid_msgs[-1]:
+					self.valid_msgs[-1] = msg
+				else:
+					self.valid_msgs.append(msg)
+			else:
+				self.valid_msgs.append(msg)
+
+			if time.time() - self.time_since_last_edit > 2:
+				self.time_since_last_edit = time.time()
+				asyncio.run_coroutine_threadsafe(self.update_msg(), self.async_loop)
+
+	async def update_msg(self, new_msg=False):
+		if new_msg:
+			self.valid_msgs.append(new_msg)
+		await self.prog_msg.edit(content="\n".join(self.valid_msgs))
 
 	def debug(self, msg):
 		self.push_to_output(msg)
@@ -65,35 +88,37 @@ async def on_message(message):
 		if '!yt' in message.content:
 			videourl = message.content.split('!yt ')[1]
 
-			prog_msg_str = f'Attempting to DL: <{videourl}>'
-			print(prog_msg_str, flush=True)
-			prog_msg = await message.channel.send(prog_msg_str)
-
-			logger = Logger()
-
-			opts = ydl_opts.copy()
-			opts['logger'] = logger
-			ydl_opts_str = str(ydl_opts).replace('_', '\_')
+			msg_content = f'Attempting to DL: <{videourl}>'
+			print(msg_content, flush=True)
+			prog_msg = await message.channel.send(msg_content)
 
 			loop = asyncio.get_running_loop()
+
+			logger = Logger(prog_msg, msg_content, loop)
+			opts = ydl_opts.copy()
+			opts['logger'] = logger
+			ydl_opts_str = str(ydl_opts)
+
 			with yt_dlp.YoutubeDL(opts) as ydl:
 				info = await loop.run_in_executor(None, lambda: ydl.extract_info(videourl, download=False))
 				title = info['title']
 				video_id = info['id']
-				msg_content = f"Attempting to DL: <{videourl}>\nTitle: {title}\nOptions: {ydl_opts_str}\nDownloading..."
-				await prog_msg.edit(content=msg_content)
+				msg_content = f'Title: {title}\nOptions: {ydl_opts_str}\nDownloading...'.replace('_', '\_')
+
+				await logger.update_msg(msg_content)
+
 				path = title + video_id
-				video_string_hash = urlsafe_b64encode(sha256(path.encode('utf-8')).digest()).decode('utf-8')
+				video_string_hash = urlsafe_b64encode(sha1(path.encode('utf-8')).digest()).decode('utf-8')[:-1]
 				path = 'downloads/' + video_string_hash + '/'
 				opts['outtmpl']['default'] = opts['outtmpl']['default'].replace('downloads/', path, 1)
 				os.makedirs(path, exist_ok=True)
 
 			with yt_dlp.YoutubeDL(opts) as ydl:
 				info = await loop.run_in_executor(None, lambda: ydl.extract_info(videourl, download=True))
-				log = '\n'.join(logger.output_msgs)
 				filename = await loop.run_in_executor(None, lambda: ydl.prepare_filename(info))
-				msg_content += f'\n{log}\nOutput:\n{filename}'
-				await prog_msg.edit(content=msg_content, suppress=True)
+
+				await logger.update_msg("Download completed.")
+
 				if PUBLIC_ADDRESS and PUBLIC_ADDRESS != 'YourPublicWebAddressHere':
 					target = filename.replace('downloads/', '', 1)
 					ddl_target = filename.replace('downloads/', 'ddl/', 1)
@@ -101,10 +126,10 @@ async def on_message(message):
 					public_filename = f"http://{PUBLIC_ADDRESS}/{urlencode(target)}"
 					ddl_filename = f"http://{PUBLIC_ADDRESS}/{urlencode(ddl_target)}"
 					
-					res_msg = await message.channel.send(f'[{title}]({public_filename}) ([download](<{ddl_filename}>))')
+					await message.channel.send(f'[{title}]({public_filename}) ([download](<{ddl_filename}>))')
 				else:
 					public_filename = filename
-					res_msg = await message.channel.send(f'{public_filename}')
+					await message.channel.send(f'{public_filename}')
 
 			output_info = {}
 			output_info['folder'] = video_string_hash
@@ -125,7 +150,14 @@ async def on_message(message):
 				json.dump(files_arr, f, indent=4)
 				f.truncate()
 	except Exception as e:
+		print('Error attempting to download video:', flush=True)
 		print(e, flush=True)
+		try:
+			await message.channel.send(e)
+		except Exception as e:
+			print('Error attempting to send exception as a message:', flush=True)
+			print(e, flush=True)
+			pass
 		pass
 
 client.run(TOKEN)
